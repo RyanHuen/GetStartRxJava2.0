6530556f3df60dab497e51c5c842d397b7c349e4
xuanweihong <xuanweihong@phoenixos.com>
2017-01-11 15:46:33 +0800

    添加注释

--- observerdownload/src/main/java/com/chaozhuo/observerdownload/observer/base/Subject.java
+++ observerdownload/src/main/java/com/chaozhuo/observerdownload/observer/base/Subject.java
@@ -9,16 +9,32 @@ import java.util.List;
  */
 
 public abstract class Subject {
+    /**
+     * 用来保存注册的观察者对象
+     */
     public static List<Observer> OBSERVERS_LIST = new ArrayList<>();
 
+    /**
+     * 注册观察者对象
+     * 
+     * @param observer 观察者对象
+     */
     public static void attach(Observer observer) {
         OBSERVERS_LIST.add(observer);
     }
 
+    /**
+     * 删除观察者对象
+     * 
+     * @param observer 观察者对象
+     */
     public static void dettach(Observer observer) {
         OBSERVERS_LIST.remove(observer);
     }
 
+    /**
+     * 通知所有注册的观察者对象
+     */
     public void notifyAllObservers(int progress) {
         for (int i = 0; i < OBSERVERS_LIST.size(); i++) {
             Observer observer = OBSERVERS_LIST.get(i);
